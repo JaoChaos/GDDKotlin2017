1035b8f4e407c1ede53a4cd1e0a5435bc7e9dfa3
JaoChaos <juan.anaya.ortiz@gmail.com>
2017-09-27 03:20:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jaochaos/gdggranada/Main.kt
@@ -0,0 +1,12 @@
+package com.example.jaochaos.gdggranada
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Main : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
